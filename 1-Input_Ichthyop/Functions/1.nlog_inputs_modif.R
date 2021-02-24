a58587955168fc08941c46422d75ca500363cdf0
adupaix <amael.dupaix@ens-lyon.fr>
2021-02-24 16:13:11 +0100

    Added header + cleaned script

--- 1-Input_Ichthyop/Functions/1.nlog_inputs_modif.R
+++ 1-Input_Ichthyop/Functions/1.nlog_inputs_modif.R
@@ -1,3 +1,12 @@
+#'#*******************************************************************************************************************
+#'@author : Amael DUPAIX
+#'@update : 2021-02-24
+#'@email : 
+#'#*******************************************************************************************************************
+#'@description :  Main function reading river/mangrove data and generating Ichthyop input points
+#'#*******************************************************************************************************************
+#'@revision
+#'#*******************************************************************************************************************
 
 
 source(file.path(FUNC_PATH,'1.1.subfunctions.pre.R'))
@@ -22,12 +31,6 @@ source(file.path(FUNC_PATH,'1.3.subfunctions.post.R'))
 ### input_method (chr): either "onMask" put points on the closest point on the      #
 #                                     current product                               #
 #                      or "kFromCoast" put points at dist km from the coast         #
-### thr_disch (chr or num): threshold of river discharge. If "mean", keep only the  #
-#                          rivers with discharge > to overall mean                  #
-#                          If numerical, keep only the rivers with discharge >= to  #
-#                          thr_disch (value in m3/s)                                #
-#    CHANGED: THE PART ON THE THRESHOLD OF DISCHARGE WAS COMMENTED, THE FILTER IS   #
-#             NOW APPLIED AFTER THE ICHTHYOP SIMULATION                             #
 #                                                                                   #
 ### dist (num) : distance from the coast at which we want the points (in deg)       #
 ### curr_prod (chr): which current mask product to use if put the input points on   #
@@ -45,7 +48,6 @@ input.nlog <- function(DATA_PATH, OUTPUT_PATH, RESOURCE_PATH,
                        return_format = "sf",
                        input_location = c("river","mangrove"),
                        input_method = c("onMask","kFromCoast"),
-                       # thr_disch = 10, # used if input_location == river
                        dist = 1, # used if input_method == kFromCoast
                        curr_prod = c("oscar","nemo"), # used if input_method == onMask
                        Parallel = c(T, 1/2) # wether to run in Parallel, used if input_method == kFromCoast
@@ -79,29 +81,6 @@ input.nlog <- function(DATA_PATH, OUTPUT_PATH, RESOURCE_PATH,
     st_centroid() -> entry_points
     
     toc()
-    
-    # cat("### FILTERING DATA WITH DISCHARGE THRESHOLD\n")
-    # tic(msg = "# FILTERING DATA WITH DISCHARGE THRESHOLD")
-    # if(thr_disch == "mean"){
-    #   # keep only the segment with a mean yearly discharge >= the mean of all rivers
-    #   rivers_IO_f %>% dplyr::filter(dis_m3_pyr >= mean(dis_m3_pyr)) %>%
-    #     # NEXT_DOWN: id of the next downstream line segment (value 0 indicates a line with no downstream connection)
-    #     # ENDORHEIC: indicator for endorheic basins (ie without surface flow connection to the ocean). 0 = not part of an endorheic bassin
-    #     # cf. HydroRIVERS_TechDoc_v10.pdf
-    #     dplyr::filter(NEXT_DOWN == 0 & ENDORHEIC == 0) -> rivers_IO_f
-    #   thr_disch <- round(mean(rivers_IO_f$dis_m3_pyr),2)
-    # } else if (is.numeric(thr_disch)){
-    #   # keep only the segment with a mean yearly discharge >= the threshold
-    #   rivers_IO_f %>% dplyr::filter(dis_m3_pyr >= thr_disch) %>%
-    #     # NEXT_DOWN: id of the next downstream line segment (value 0 indicates a line with no downstream connection)
-    #     # ENDORHEIC: indicator for endorheic basins (ie without surface flow connection to the ocean). 0 = not part of an endorheic bassin
-    #     # cf. HydroRIVERS_TechDoc_v10.pdf
-    #     dplyr::filter(NEXT_DOWN == 0 & ENDORHEIC == 0)-> rivers_IO_f
-    # }
-    # toc()
-    
-    
-    
     
     
     ### For simulations from the mangroves
@@ -140,7 +119,7 @@ input.nlog <- function(DATA_PATH, OUTPUT_PATH, RESOURCE_PATH,
     # besoin de la ligne precise, parce que sinon certaines arrivees par les iles
     # disparaissent du jeu de donnees(a cause du buffer dans la fct closest.point)
     
-    load(file.path(RESOURCE_PATH, "coastline10.rda")) # highres coastline, downloaded at : https://github.com/ropensci/rnaturalearthhires/tree/master/data
+    load(file.path(RESOURCE_PATH, "coastline10.rda")) # highres coastline, downloaded at : https://github.com/ropensci/rnaturalearthhires/tree/master/data (last accessed 2021-02-25)
     coastline10 <- st_as_sf(coastline10)
     coastline_points <- st_line_sample(st_transform(coastline10, 3857), density = set_units(3, km)) %>% # je ne sais pas pourquoi, en mettant 3km, les points sont espacés d'environ 1km...
       st_transform(crs = 4326) %>% # repasse a la projection initiale
@@ -220,28 +199,7 @@ input.nlog <- function(DATA_PATH, OUTPUT_PATH, RESOURCE_PATH,
                              coords = c("X","Y"),
                              crs = 4326)
     
-    # input_points <- list()
-    # pb <- progress_bar$new(total = dim(entry_points)[1])
-    # for (i in 1:dim(entry_points)[1]){
-    #   #method1
-    #   # input_points[[i]] <- sea_points[which.min(st_distance(entry_points[i,], sea_points)),] %>%
-    #   #   st_as_sf() %>% # transform in sf object
-    #   #   bind_cols(st_drop_geometry(entry_points[i,])) #add the columns of entry_points[i,]
-    #   
-    #   
-    #   #method2 (limiting distance calculation using a buffer)
-    #   #get the grid points contained in a buffer of 0.5 deg around the point
-    #   #
-    #   input_points[[i]] <- closest.point(entry_points[i,], sea_points)
-    #   
-    #   pb$tick()
-    #   
-    # }
-    # input_points <- bind_rows(input_points)
-    
     toc()
-    
-    
     
   }
   
@@ -258,10 +216,6 @@ input.nlog <- function(DATA_PATH, OUTPUT_PATH, RESOURCE_PATH,
     input_coords_id <- input_coords
   }
   
-  ## round the numbers to 6 digits, to have the exact same values in the .txt and in the ichthyop output for post-treatment
-  # input_coords <- apply(input_coords, 2, function(x) format(round(x, 6), nsmall = 6))
-  # input_coords_id[,c("X","Y")] <- apply(input_coords_id[,c("X","Y")], 2, function(x) format(round(x, 6), nsmall = 6))
-  
   if (save==T){
     
     # create the name on the dir where files will be saved, depending on method and input location
@@ -271,9 +225,6 @@ input.nlog <- function(DATA_PATH, OUTPUT_PATH, RESOURCE_PATH,
     } else if (input_method == "onMask"){
       dir_name <- paste0(dir_name, curr_prod,"_mask")
     }
-    # if (input_location == "river"){
-    #   dir_name <- paste0(dir_name, "_thr_disch_",thr_disch,"m3s-1")
-    # }
     
     cat("\n### Saving input locations\n")
     dir_path <- file.path(OUTPUT_PATH, dir_name)
