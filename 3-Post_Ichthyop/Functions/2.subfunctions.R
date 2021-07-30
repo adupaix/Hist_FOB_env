987d51a0411ca2c52178140f29901d57c2fd0405
adupaix <amael.dupaix@ens-lyon.fr>
2021-07-30 16:02:01 +0200

    correction subfunction 4 + add 6 and 7

--- 3-Post_Ichthyop/Functions/2.subfunctions.R
+++ 3-Post_Ichthyop/Functions/2.subfunctions.R
@@ -14,7 +14,7 @@
 #'read the link table generate in the sub-routine 1
 read.link_cover_input <- function(OUTPUT_PATH, sim_name){
   
-  link_table <- read.csv(file.path(OUTPUT_PATH, sim_name, "number_of_cover_points_per_input_point.csv"), header = T)
+  link_table <- read.csv(file.path(OUTPUT_PATH, sim_name, "1.nb_cover", "number_of_cover_points_per_input_point.csv"), header = T)
   
   return(link_table)
 }
@@ -81,7 +81,7 @@ get.precipitations <- function(DATA_PATH,
 #' get the information on the rivers associated with the input point of interest
 get.associated.rivers <- function(sim_input_path,
                                   n_cover_per_river,
-                                  filtered,
+                                  embouchures,
                                   point){
   
   rivers <- read.table(file.path(sim_input_path, "Link_table.txt"), header = T)
@@ -98,9 +98,9 @@ get.associated.rivers <- function(sim_input_path,
     point$rivers$dis_m3_pmx <- c()
     for (i in 1:length(point$rivers)){
       point$rivers$cover[i] <- n_cover_per_river$nb_river_cover_points[n_cover_per_river$MAIN_RIV == point$rivers[i]]
-      point$rivers$dis_m3_pyr[i] <- filtered$dis_m3_pyr[filtered$MAIN_RIV == point$rivers$ids[i]]
-      point$rivers$dis_m3_pmn[i] <- filtered$dis_m3_pmn[filtered$MAIN_RIV == point$rivers$ids[i]]
-      point$rivers$dis_m3_pmx[i] <- filtered$dis_m3_pmx[filtered$MAIN_RIV == point$rivers$ids[i]]
+      point$rivers$dis_m3_pyr[i] <- embouchures$dis_m3_pyr[embouchures$MAIN_RIV == point$rivers$ids[i]]
+      point$rivers$dis_m3_pmn[i] <- embouchures$dis_m3_pmn[embouchures$MAIN_RIV == point$rivers$ids[i]]
+      point$rivers$dis_m3_pmx[i] <- embouchures$dis_m3_pmx[embouchures$MAIN_RIV == point$rivers$ids[i]]
     }
   } else {
     point$rivers$cover <- 0
@@ -115,18 +115,75 @@ get.associated.rivers <- function(sim_input_path,
 #'@sub-function 5
 #'***************
 #' get the number of forest cover points associated with the input point
-#' forest_surface is in m2
+#' forest_surface is in km2
 get.number.of.cover.points <- function(link_table, point){
   
   point$nb_coastal_cover_points <- link_table$nb_coastal_cover_points[link_table$id_curr == as.numeric(point$id)]
   
   point$nb_cover_points <- link_table$nb_cover_points[link_table$id_curr == as.numeric(point$id)]
   
-  point$forest_surface <- point$nb_cover_points * 900
+  point$forest_surface <- point$nb_cover_points * 900 / 10^6
   
   return(point)
 }
 
-#'
-#'
-#'
+
+#' @sub-function 6
+#' ***************
+#' weight point$data according to the method used
+add.weight <- function(point, weight_method){
+  
+  # method 1 : don't apply any weight
+  if (weight_method == 1){
+    weight <- 1
+    
+    # method 2 : apply a weight depending on the water discharge of the river
+  }  else if (weight_method==2){
+    weight <- sum(point$rivers$dis_m3_pyr)
+    
+    # method 3: apply a weight depending on the surface of cover associated with the input point
+  } else if (weight_method == 3){
+    weight <- point$nb_cover_points
+    
+    # method 4: apply a weight depending on the area covered by forest in the river basins
+  } else if (weight_method == 4){
+    weight <- point$nb_coastal_cover_points + sum(point$rivers$cover * point$rivers$dis_m3_pyr)
+  }
+  
+  point$data <- point$data * weight
+  
+  return(point)
+}
+
+
+#'@sub-function 7
+#'***************
+#' apply the mortality on the density map (modify point$data)
+
+apply.mortality <- function(point, ltime, ltime_method, sd = 30){
+  
+  timestep <- as.POSIXct(dimnames(point$data)[[3]][2]) - as.POSIXct(dimnames(point$data)[[3]][1])
+  
+  total_time_sim <- dim(point$data)[3]
+  
+  if (ltime_method == 1){
+    
+    x <- seq(1, total_time_sim, 1)
+    
+    prop_alive <- 1 - cumsum(dnorm(x, ltime/as.numeric(timestep), sd/as.numeric(timestep)))
+    
+  } else if (ltime_method == 2){
+    
+    prop_alive <- c( rep(1, ltime/as.numeric(timestep)), rep(0, (total_time_sim-ltime)/as.numeric(timestep)))
+    
+  }
+  
+  # multiply the matrix of each timestep by the proportion of particles alive
+  point$data <- sweep(point$data, 3, prop_alive, "*")
+  
+  # delete dates when no particles are left, to gain space
+  has_particles_left <- apply(point$data, 3, sum) != 0
+  point$data <- point$data[,,which(has_particles_left)]
+  
+  return(point)
+}
