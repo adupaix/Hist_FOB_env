ffc05270239ee6d7137e8e3f133ec030d59af549
adupaix <amael.dupaix@ens-lyon.fr>
2021-08-06 19:11:54 +0200

    working version, saving info on points which have a weight different from zero

--- 3-Post_Ichthyop/Sub-routines/2.information_on_point.R
+++ 3-Post_Ichthyop/Sub-routines/2.information_on_point.R
@@ -3,92 +3,141 @@
 #'@update : 2021-07-21
 #'@email : 
 #'#*******************************************************************************************************************
-#'@description :  Reads the rds files from the simulation from one point and weight it according to the chosen
-#'  method
+#'@description :  For each release point, get the information for weighting (cover surface, precipitations and 
+#'  associated rivers with discharge)
 #'#*******************************************************************************************************************
 #'@revision
 #'#*******************************************************************************************************************
 
 
-#'@arguments
-#'**********
+cat(bold("\n\n2. Getting information on input points\n"))
+cat("  - Weighting method:",weight_method, "\n\n")
 
-sim_output_path <- file.path(DATA_PATH, "Output_Ichthyop", sim_name)
+#' get the table containing the number of cover points associated with each river
+n_cover_per_river <- read.csv(file.path(OUTPUT_PATH, sim_name, year, "1.nb_cover", "n_cover_per_river.csv"),
+                              header = T)
 
-point <- list()
+#' Get the table linking river ids with input point ids
+link_river_input <- read.table(file.path(sim_input_path, "Link_table.txt"), header = T)
+#' keep only the rivers above the threshold in link_river_input
+link_river_input %>% dplyr::filter(MAIN_RIV %in% embouchures$MAIN_RIV) -> link_river_input
 
-point$id <- "00101"
+sub_dirs <- list.files(sim_output_path)
 
-n_points_per_dir = 28*8*5
+non_null_points <- list()
 
-#'
-
-num_sub_dir <- floor((as.numeric(point$id)-1)/ n_points_per_dir) + 1
-num_sub_dir <- paste0(substr("00", 1, nchar("00")-nchar(num_sub_dir)),
-                      num_sub_dir)
-
-fnames <- list.files(file.path(sim_output_path,
-                               paste0("points_", num_sub_dir),
-                               point$id),
-                     pattern = "rds")
-
-link_table <- read.link_cover_input(OUTPUT_PATH, sim_name)
-
-n_cover_per_river <- read.csv(file.path(OUTPUT_PATH, sim_name, "1.nb_cover", "n_cover_per_river.csv"),
-                                header = T)
-# n_cover_per_river <- read.table(file.path(OUTPUT_PATH, sim_name, "1.nb_cover", "n_cover_per_river.txt"),
-#                               header = T)
-
-# read rds file
-i=1
-point$data <- readRDS(file.path(sim_output_path, paste0("points_", num_sub_dir), point$id, fnames[i]))
-
-sim_input_path <- file.path(DATA_PATH, "Input_Ichthyop", paste0(input_location, "_nlog_input_", forcing, "_", input_method))
+pb <- progress_bar$new(format = "[:bar] :percent | Sub directory :current / :total",
+                       total = length(sub_dirs))
 
 
-#'@get_information_on_release_point
-#'***********************************
-
-
-# get release coordinates
-point <- get.coords.release(sim_input_path,
-                            point)
-
-# get release date
-
-point$release_date <- as.POSIXct(dimnames(point$data)[[3]][1])
-
-# get precipitations
-
-point <- get.precipitations(DATA_PATH, point)
-
-# get rivers and associated discharge + cover
-
-point <- get.associated.rivers(sim_input_path, n_cover_per_river, embouchures, point)
-
-
-# get forest cover
-
-point <- get.number.of.cover.points(link_table, point)
-
-if (point$nb_cover_points != point$nb_coastal_cover_points + sum(point$rivers$cover)){
-  stop("Error")
+for (i in 1:length(sub_dirs)){
+  
+  pb$tick()
+  
+  non_null_points[[i]] <- data.frame(matrix(ncol = 3))
+  
+  dir.create(file.path(output_path_2, sub_dirs[i]), recursive = T, showWarnings = F)
+  
+  rds_files <- list.files(file.path(sim_output_path, sub_dirs[i]), recursive = T, pattern = ".rds")
+  rds_files <- grep("infos", rds_files, invert = T, value = T)
+  
+  # rds_files <- rds_files[1:5000]
+  
+  #'@arguments
+  #'**********
+  
+  registerDoParallel(cores = nb_cores)
+  
+  
+  non_null_points[[i]] <- foreach(k = 1:length(rds_files),
+                               .combine = rbind) %dopar% {
+                                 
+                                 point <- list()
+                                 
+                                 point$id <- sub("/.*","",rds_files[k])
+                                 
+                                 fname <- sub(".*/","",rds_files[k])
+                                 
+                                 
+                                 #'@comment trouver une solution ici pour accelerer (parallelisation?). La lecture du fichier prend ~0.016s, ce qui amene a 2h45 pour lire tous les fichiers
+                                 #' ou sinon, lecture apres, sauvegarde juste les infos sur le point
+                                 # point$data <- readRDS(file.path(sim_output_path, paste0("points_", num_sub_dir), point$id, fnames[i]))
+                                 
+                                 
+                                 #'@get_information_on_release_point
+                                 #'***********************************
+                                 
+                                 
+                                 # get release coordinates
+                                 point <- get.coords.release(sim_input_path,
+                                                             point)
+                                 
+                                 # get release date
+                                 
+                                 point$release_date <- as.Date(sub("\\..*", "", sub(".*_", "", fname)))
+                                 
+                                 # get precipitations
+                                 
+                                 point <- get.precipitations(DATA_PATH, point)
+                                 
+                                 # get rivers and associated discharge + cover
+                                 
+                                 point <- get.associated.rivers(link_river_input, n_cover_per_river, embouchures, point)
+                                 
+                                 
+                                 # get forest cover
+                                 
+                                 point <- get.number.of.cover.points(nb_cover_per_input, point)
+                                 
+                                 if (round(point$nb_cover_points) != round(point$nb_coastal_cover_points + sum(point$rivers$cover))){
+                                   stop("Error: total number of cover points does not correspond to sum of coastal and river associated points")
+                                 }
+                                 
+                                 
+                                 # weight density map
+                                 
+                                 point <- add.weight(point, weight_method)
+                                 
+                                 
+                                 # apply mortality
+                                 
+                                 # point <- apply.mortality(point, ltime, ltime_method, sd = ltime_sd)
+                                 
+                                 
+                                 if (point$weight != 0){
+                                   # fill in non_null_points
+                                   non_null_point <- c(sub_dirs[i],
+                                                       point$id,
+                                                       # the date will be changed to character (hence keep the number of days since 1990-01-01, to choose the time origin)
+                                                       as.numeric(difftime(point$release_date, as.Date("1990-01-01"), units = "days")))
+                                   
+                                   # Saving the point object
+                                   outfile_name <- paste0(point$id, "_", point$release_date, "_infos.rds")
+                                   
+                                   out_dir <- file.path(output_path_2, sub_dirs[i], point$id)
+                                   dir.create(out_dir, showWarnings = F)
+                                   
+                                   saveRDS(point, file.path(out_dir, outfile_name))
+                                 } else {
+                                   non_null_point <- c(NA,NA,NA)
+                                 }
+                                 
+                                 non_null_point
+                                 
+                               }
+  
+  
+  registerDoSEQ()
+  
+  # change format of non_null_points
+  non_null_points[[i]] <- as.data.frame(non_null_points[[i]])
+  names(non_null_points[[i]]) <- c("sub_dir", "point_id", "release_date")
+  
+  #' format release dates back to date
+  non_null_points[[i]] %>% dplyr::filter(!is.na(release_date)) -> non_null_points[[i]]
+  non_null_points[[i]]$release_date <- as.Date("1990-01-01")+ as.difftime(as.numeric(non_null_points[[i]]$release_date), units = "days")
+  
+  
 }
 
-
-# weight density map
-
-point <- add.weight(point, weight_method)
-
-
-# apply mortality
-
-point <- apply.mortality(point, ltime, ltime_method, sd = ltime_sd)
-
-
-
-# Saving the point object
-
-outfile_name <- paste0(point$id, "_", point$release_date, ".rds")
-
-writeRDS(point, file.path(output_path_2, paste0("points_", num_sub_dir), point$id, outfile_name))
+non_null_points <- bind_rows(non_null_points)
